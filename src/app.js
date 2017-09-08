0d9c5fdae46f45b3f60298d430cd618417006ce8
Logan Campbell <lcampbellmcpherson@zendesk.com>
2017-09-08 16:06:40 +1000

    connected lovefield to domvm

--- src/app.js
+++ src/app.js
@@ -2,30 +2,37 @@ import schema from './schema';
 import seed from './seed';
 import domvm from 'domvm';
 
-async function roundTrip() {
-  const db = await schema().connect();
-  await seed(db);
+const el = domvm.defineElement;
 
-  const item = db.getSchema().table("Item");
-  const results = await db.select().from(item).where(item.done.eq(false)).exec();
-
-  results.forEach(function(row) {
-    console.log(row['description'], 'before', row['deadline']);
-  });
+function ItemList(vm, data) {
+  return (
+    el("div", [
+      data.length == 0
+        ? el("p", "None")
+        : el("ul", data.map((item) => el("li", item.description)))
+    ])
+  );
 }
 
-roundTrip();
+async function main() {
+  // LOVEFIELD
+  const db = await schema().connect();
+  const item = db.getSchema().table("Item");
+  const query = db.select().from(item).where(item.done.eq(false));
 
-function ui() {
-  const el = domvm.defineElement;
+  // DOMVM
+  const vm = domvm.createView({render: ItemList}, []).mount(document.body);
 
-  function HelloView(vm, data) {
-    return el("h1", {style: "color: red;"}, "Hello " + data.name);
+  // BOTH
+  async function updateDomvm(changes) {
+    const results = await query.exec();
+    vm.update(results, false);
+    console.log(results);
   }
 
-  const data = {name: "Leon"};
+  db.observe(query, updateDomvm);
 
-  domvm.createView({render: HelloView}, data).mount(document.body);
+  seed(db);
 }
 
-ui();
+main();

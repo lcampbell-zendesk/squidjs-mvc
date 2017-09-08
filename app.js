cfa8dd42abd0a79b329cd7646de208f8b0e10e8a
Logan Campbell <lcampbellmcpherson@zendesk.com>
2017-09-08 14:30:28 +1000

    async lovefield

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-require("babel-core/register");
-require("babel-polyfill");
+import 'babel-core/register';
+import 'babel-polyfill';
+import lf from 'lovefield';
 import bar from './bar';
 
 async function foo() {
@@ -8,3 +9,37 @@ async function foo() {
 }
 
 foo();
+
+// ---------------------------------------------------------------------- //
+
+var schemaBuilder = lf.schema.create('todo', 1);
+
+schemaBuilder.createTable('Item').
+  addColumn('id', lf.Type.INTEGER).
+  addColumn('description', lf.Type.STRING).
+  addColumn('deadline', lf.Type.DATE_TIME).
+  addColumn('done', lf.Type.BOOLEAN).
+  addPrimaryKey(['id']).
+  addIndex('idxDeadline', ['deadline'], false, lf.Order.DESC);
+
+let db, item;
+(async () => {
+  db = await schemaBuilder.connect();
+
+  item = db.getSchema().table("Item");
+
+  const row = item.createRow({
+    "id": 1,
+    "description": "Get a cup of coffee",
+    "deadline": new Date(),
+    "done": false
+  });
+
+  await db.insertOrReplace().into(item).values([row]).exec();
+
+  const results = await db.select().from(item).where(item.done.eq(false)).exec();
+
+  results.forEach(function(row) {
+    console.log(row['description'], 'before', row['deadline']);
+  });
+})();

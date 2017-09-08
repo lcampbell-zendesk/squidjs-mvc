0a956d0c584ca8b10bd517e024a19c25ebd43aba
Logan Campbell <lcampbellmcpherson@zendesk.com>
2017-09-08 14:07:26 +1000

    babel env and webpack basics

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}

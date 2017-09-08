33d349441570adc5ab9713ccb6776cc8ba6dc912
Logan Campbell <lcampbellmcpherson@zendesk.com>
2017-09-08 15:00:55 +1000

    domvm hello world

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'babel-polyfill';
 import lf from 'lovefield';
 import createItem from './schema';
 import seed from './seed';
+import domvm from 'domvm';
 
 async function roundTrip() {
   var schemaBuilder = lf.schema.create('todo', 1);
@@ -21,3 +22,19 @@ async function roundTrip() {
 }
 
 roundTrip();
+
+function ui() {
+  var el = domvm.defineElement;
+
+  var HelloView = {
+    render: function(vm, data) {
+      return el("h1", {style: "color: red;"}, "Hello " + data.name);
+    }
+  };
+
+  var data = {name: "Leon"};
+
+  domvm.createView(HelloView, data).mount(document.body);
+}
+
+ui();
